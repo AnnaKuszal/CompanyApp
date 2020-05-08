9c019a7afed1ea70f35295c1bfb61a669a3dae1b
Anna Kuszal <anna.kuszal@o2.pl>
2020-05-08 20:27:43 +0200

    Connect sever to db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoClient = require('mongodb').MongoClient;
 
 const employeesRoutes = require('./routes/employees.routes');
 const departmentsRoutes = require('./routes/departments.routes');
@@ -14,10 +15,22 @@ app.use('/api', employeesRoutes);
 app.use('/api', departmentsRoutes);
 app.use('/api', productsRoutes);
 
-app.use((req, res) => {
-  res.status(404).send({ message: 'Not found...' });
-})
+mongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  if (err){
+    console.log(err);
+  }
+  else {
+    console.log('Successfully connected to the database');
+  }
 
-app.listen('8000', () => {
-  console.log('Server is running on port: 8000');
+  app.use((req, res) => {
+    res.status(404).send({ message: 'Not found...' });
+  })
+  
+  app.listen('8000', () => {
+    console.log('Server is running on port: 8000');
+  });
+  
 });
+
+
