0db113e491e7676672b8a6511cfb0a0a1d31eb93
Anna Kuszal <anna.kuszal@o2.pl>
2020-05-10 18:43:12 +0200

    Use reference to department document in employees routes

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -5,7 +5,7 @@ const Employee = require('../models/employee.model');
 
 router.get('/employees', async (req, res) => {
   try {
-    res.json(await Employee.find());
+    res.json(await Employee.find().populate('department'));
   }
   catch(err) {
     res.status(500).json({ message: err });
@@ -17,9 +17,9 @@ router.get('/employees/random', async (req, res) => {
   try {
     const count = await Employee.countDocuments();
     const rand = Math.floor(Math.random() * count);
-    const dep = await Employee.findOne().skip(rand);
-    if(!dep) res.status(404).json({ message: 'Not found' });
-    else res.json(dep);
+    const employee  = await Employee.findOne().skip(rand).populate('department');
+    if(!employee) res.status(404).json({ message: 'Not found' });
+    else res.json(employee);
   }
   catch(err) {
     res.status(500).json({ message: err });
@@ -30,9 +30,9 @@ router.get('/employees/random', async (req, res) => {
 router.get('/employees/:id', async (req, res) => {
 
   try {
-    const dep = await Employee.findById(req.params.id);
-    if(!dep) res.status(404).json({ message: 'Not found' });
-    else res.json(dep);
+    const employee = await Employee.findById(req.params.id).populate('department');
+    if(!employee) res.status(404).json({ message: 'Not found' });
+    else res.json(employee);
   }
   catch(err) {
     res.status(500).json({ message: err });
@@ -59,8 +59,8 @@ router.put('/employees/:id', async (req, res) => {
   const { firstName, lastName, department } = req.body;
 
   try {
-    const dep = await(Employee.findById(req.params.id));
-    if(dep) {
+    const employee = await(Employee.findById(req.params.id));
+    if(employee) {
       await Employee.updateOne({ _id: req.params.id }, { $set: { firstName: firstName, lastName: lastName, department: department }});
       res.json({ message: 'OK' });
     }
@@ -75,8 +75,8 @@ router.put('/employees/:id', async (req, res) => {
 router.delete('/employees/:id', async (req, res) => {
 
   try {
-    const dep = await(Employee.findById(req.params.id));
-    if(dep) {
+    const employee = await(Employee.findById(req.params.id));
+    if(employee) {
       await Employee.deleteOne({ _id: req.params.id });
       res.json({ message: 'OK' });
     }
